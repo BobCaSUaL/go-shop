60f14c75797e05d322b1c72ec7f5a9ae262a17ed
Roberto Casula <casual4free@gmail.com>
2020-09-06 12:53:19 +0200

    feat: sample cart initialstate

--- app/containers/ShoppingCart/reducer.js
+++ app/containers/ShoppingCart/reducer.js
@@ -6,7 +6,31 @@
 import produce from 'immer';
 import { DEFAULT_ACTION } from './constants';
 
-export const initialState = {};
+export const initialState = {
+  productList: {
+    'p-6005': {
+      id: 'p-6005',
+      thumbnail: null,
+      title: 'Sculpture of whatever it is',
+      price: '99.99€',
+      quantity: 2,
+      maxQuantity: 4,
+    },
+    'p-6057': {
+      id: 'p-6057',
+      thumbnail: null,
+      title: 'Statue of the three graces',
+      price: '156.06€',
+      quantity: 1,
+      maxQuantity: 3,
+    },
+    /* Ordered associative list */
+  },
+  promoCode: null,
+  VAT: null,
+  shippingMethod: null,
+  paymentMethod: null,
+};
 
 /* eslint-disable default-case, no-param-reassign */
 const shoppingCartReducer = (state = initialState, action) =>
