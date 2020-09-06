821b4328819bb04c0cd095f64275ee0aeafff795
Roberto Casula <casual4free@gmail.com>
2020-09-06 11:26:07 +0200

    chore: audit fix and project cleanup

---
+++ app/components/IconButton/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * IconButton
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function IconButton() {
+  return <div />;
+}
+
+IconButton.propTypes = {};
+
+export default IconButton;
