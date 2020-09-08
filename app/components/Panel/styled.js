bab5fd9922bbd407c0ce13abe18facb47b75bb2f
Roberto Casula <casual4free@gmail.com>
2020-09-08 22:53:21 +0200

    feat: styling the panel

--- app/components/Panel/styled.js
+++ app/components/Panel/styled.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
 export const StyledPanel = styled.div`
+  box-shadow: -2px 4px 8px 0px rgba(0, 0, 0, 0.2);
+  margin-bottom: 20px;
   .title {
+    background-color: #dadaee;
+    padding: 8px 20px;
+    margin: 0;
   }
-  .contentContainer {
+  .content-container {
+    padding: 8px 16px;
   }
 `;
